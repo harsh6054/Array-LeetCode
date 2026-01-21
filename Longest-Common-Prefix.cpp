6010de21084b6c87f1e2a4afe1486af50ec47467
Harshvardhan Patil <147519211+harsh6054@users.noreply.github.com>
2026-01-21 18:33:40 +0530

    Update Longest-Common-Prefix.cpp

--- Longest-Common-Prefix.cpp
+++ Longest-Common-Prefix.cpp
@@ -4,22 +4,7 @@ using namespace std;
 class Solution 
 {
 public:
-    string longestCommonPrefix(vector<string>& strs) 
-    {
-        if (strs.empty()) return "";
-        string prefix = strs[0];
-        for (int i = 1; i < strs.size(); i++) 
-        {
-            while (strs[i].find(prefix) != 0) 
-            {
-                prefix.pop_back();
-                if (prefix.empty())
-                    return "";
-            }
-        }
-        return prefix;
-    }
-};
+   
 
 int main() 
 {
@@ -30,3 +15,4 @@ int main()
     cout << "Output 2: " << sol.longestCommonPrefix(strs2) << endl;
     return 0;
 }
+
